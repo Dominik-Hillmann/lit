1c4f62a63ab61facdb13e64d4273eb8047e7ab5b
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-09-19 01:05:44 +0200

    Setup for project change

--- public/electron.js
+++ public/electron.js
@@ -3,26 +3,24 @@ const path = require('path');
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 
-function createWindow() {
+const createWindow = () => {
     // Create the browser window.
-    const win = new BrowserWindow({
+    const window = new BrowserWindow({
         width: 800,
         height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
+        webPreferences: { nodeIntegration: true },
     });
 
     // and load the index.html of the app.
     // win.loadFile("index.html");
-    win.loadURL(
-        isDev
-            ? 'http://localhost:3000'
-            : `file://${path.join(__dirname, '../build/index.html')}`
+    window.loadURL(
+        isDev ? 
+            'http://localhost:3000' : 
+            `file://${path.join(__dirname, '../build/index.html')}`
     );
     // Open the DevTools.
     if (isDev) {
-        win.webContents.openDevTools({ mode: 'detach' });
+        window.webContents.openDevTools({ mode: 'detach' });
     }
 }
 
@@ -41,7 +39,8 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
+    const noWindowsOpen = BrowserWindow.getAllWindows().length === 0;
+    if (noWindowsOpen) {
         createWindow();
     }
 });
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import '../css/Sidebar.css';
 
-
-
 export default class Sidebar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    handleProject1() {
+        console.log('Test 1');
+        this.props.changeProject();
+    }
+
+    handleProject2() {
+        console.log('Test 2');
+        this.props.changeProject();
+    }
+
+
     render() {
-        return (<nav> </nav>);
+        return (
+            <nav>
+                <p
+                    onClick={() => this.handleProject1()}
+                >
+                    Test project 1
+                </p>
+                <p
+                    onClick={() => this.handleProject2()}
+                >
+                    Test project 2
+                </p>
+            </nav>
+        );
     }
 }
