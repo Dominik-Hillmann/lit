cffb88067d186736f72df8b5c03c8ceb5d95cfb1
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-10-10 23:49:27 +0200

    Started source details

---
+++ src/components/SourceDetails.jsx
@@ -0,0 +1,16 @@
+import react, { Component } from 'react';
+
+import '../css/SourceDetails.css';
+
+export default class SourceDetails extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
