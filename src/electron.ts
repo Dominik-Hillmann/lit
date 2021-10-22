524532653653e3b33bdc276c42f1c67efc821db3
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-10-22 23:04:35 +0200

    Setup Electron with TypeScript done

---
+++ src/electron.ts
@@ -0,0 +1,13 @@
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = (): void => {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: { nodeIntegration: true }
+    });
+
+    win.loadFile('../public/index.html');
+}
+
+app.on('ready', createWindow);
